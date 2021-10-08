55c0e5e061bf9a5da034dbce906a9f3349779f96
MohammadReza Ghadimi <mmr.ghadimi@gmail.com>
2021-10-09 00:21:38 +0330

    using admin panel, adding new items to this panel

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
