a27f1162d5dd5c5a69a92e5b3b7e03f45d070404
MohammadReza Ghadimi <mmr.ghadimi@gmail.com>
2021-10-09 00:55:31 +0330

    show a database to public user

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, DiscountOffer
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
 
-admin.site.register(Product)
+
+class DiscountOfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(DiscountOffer, DiscountOfferAdmin)
--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 
 # /products -> index
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    # return HttpResponse('Hello World')
+    return render(request, 'index.html',
+                  {'pro': products})
 
 
 def new_products(request):
